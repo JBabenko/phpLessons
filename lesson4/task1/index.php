2e7fa1ec90fbe333c026cff1389fcbf89c661706
JBabenko <klazomania@yandex.ru>
2019-05-02 18:46:19 +0300

    task 1 done

---
+++ lesson4/task1/index.php
@@ -0,0 +1,32 @@
+<?php
+  $pictures = ["276448-Berserker.jpg", "dwoilp3BVjlE.jpg", "kotenok.jpg"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Фотогалерея</title>
+</head>
+
+<body>
+  <div class="gallery">
+    <?php foreach($pictures as $item) { ?>
+    <a class="gallery-link" href="./img/<?=$item?>" target="_blank">
+      <img class="gallery-img" src="./img/<?=$item?>" alt="">
+    </a>
+    <?}?>
+  </div>
+  <style>
+    .gallery-link {
+      display: inline-block;
+    }
+    .gallery-img {
+      width: 150px;
+    }
+  </style>
+</body>
+</html>
